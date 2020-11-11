d45ab8df72a9c6bb94d776301c557808926f2b17
kenngran <kenn.gran@gmail.com>
2020-11-11 16:37:48 +0100

    Created Dato class

---
+++ Andre_Oppgaver/Arv/Dato.java
@@ -0,0 +1,26 @@
+package Andre_Oppgaver.Arv;
+
+public class Dato {
+    private int dag, måned, år;
+
+    public Dato( int dag, int måned, int år ) {
+      this.dag = dag;
+      this.måned = måned;
+      this.år =år;
+    }
+
+    private static String månedsnavn( int mnd ) {
+      String[] navn = { "januar", "februar", "mars", "april", "mai", 
+        "juni", "juli", "august", "september", "oktober", "november", 
+        "desember" };
+      if ( mnd > 0 && mnd < 13 )
+        return navn[ mnd - 1 ];
+      else
+        return "ukjent måned";
+    }
+    
+    public String toString(){
+    	// lag denne
+		return null;
+    }
+}
